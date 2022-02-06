6cd6782949a6fd78661082667ef23253e5730c38
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2022-02-06 14:53:11 +0300

    creatd a navbar

--- models.py
+++ models.py
@@ -5,11 +5,13 @@ class Pitch(db.Model):
     __tablename__ = "pitches"
     id = db.Column(db.Integer, primary_key=True)
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    heading = db.Column(db.String(255))
     description = db.Column(db.String(255))
     posted = db.Column(db.Date)
 
-    def __init__(self, category, description, posted):
-        self.category = category
+    def __init__(self, category, heading, description, posted):
+        self.category_id = category
+        self.heading = heading
         self.description = description
         self.posted = posted
 
--- views.py
+++ views.py
@@ -5,24 +5,31 @@ from datetime import date
 from run import db
 
 
-@app.route('/')
+@app.route('/add-pitch')
 def form_pitch():
     categories = Category.query.all()
     return render_template('pitch_form.html', categories=categories)
 
 
-@app.route('/submit', methods=['POST'])
+@app.route('/')
+def get_all_pitches():
+    pitches = Pitch.query.all()
+    return render_template('pitches.html', pitches=pitches)
+
+
+@app.route('/', methods=['POST'])
 def add_pitch():
     categories = Category.query.all()
     if request.method == 'POST':
         category = request.form['category']
+        heading = request.form['name']
         description = request.form['pitch']
         posted = date.today()
 
-        if category == '---select category---' or description == '':
+        if category == '---select category---' or description == '' or heading == '':
             return render_template("pitch_form.html", message="Please enter required fields", categories=categories)
         else:
-            pitch = Pitch(category, description, posted)
+            pitch = Pitch(category, heading, description, posted)
             db.session.add(pitch)
             db.session.commit()
             return render_template('pitches.html')
