42a4bd2f53025ab5438091071e900db32e957717
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2022-02-07 14:02:07 +0300

    deploy to heroku

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 export SECRET_KEY="1234567"
 export MAIL_USERNAME="joshua.barawa@student.moringaschool.com"
 export MAIL_PASSWORD="Mwa2748nda%"
-python3.8 run.py server
+python3.8 run.py serverexport
